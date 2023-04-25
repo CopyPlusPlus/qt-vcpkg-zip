8bd0f5946d2f6dce73e1439c627472eedc47a904
wy-luke <li1078797083@qq.com>
2023-04-25 16:43:13 +0800

    A shell script generating packages

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "=========================================================="
+echo "Bootstrap vcpkg"
+./vcpkg/bootstrap-vcpkg.sh
+
+package_list=(
+    "qt5-base"
+    "qt5-tools"
+    "qt5-translations"
+)
+
+echo "=========================================================="
+echo "package_list: ${package_list[@]}"
+
+echo "=========================================================="
+echo "Install packages"
+./vcpkg/vcpkg install ${package_list[@]}
+
+echo "=========================================================="
+echo "Export packages"
+./vcpkg/vcpkg export ${package_list[@]} --zip --output-dir=./ --output=my-qt5-libs
+
+echo "=========================================================="
+echo "Done"
